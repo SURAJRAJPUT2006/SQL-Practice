cc95bcb47afedeed35e5930195f9a347498edbcc
Suraj Rajput <109722721+SURAJRAJPUT2006@users.noreply.github.com>
2025-09-05 12:31:35 +0530

    Delete Queries directory

--- Queries/problem1_total_revenue.sql
+++
@@ -1,9 +0,0 @@
--- Problem 1: Calculate total revenue per product category exceeding 5000 in 2025
-
-SELECT p.category, SUM(p.price * o.quantity) AS total_revenue
-FROM Products AS p
-JOIN Orders AS o ON p.product_id = o.product_id
-WHERE YEAR(o.order_date) = 2025
-GROUP BY p.category
-HAVING SUM(p.price * o.quantity) > 5000
-ORDER BY total_revenue DESC;
